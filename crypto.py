ccaf9b7a64f009a5d81ac130145191d73e46c3c8
Sergio Romeu Agraso <sergioromagr@gmail.com>
2020-05-24 12:28:45 +0200

    Increments and decrements are available if you chose the check prices for x seconds

--- crypto.py
+++ crypto.py
@@ -27,19 +27,19 @@ def menu():
     elif option == 2:
         print("Alerts are in process, they will be out soon\n")
     elif option == 3:
-        seconds = int(input("Enter the seconds you want to monitor the prices, keep in mind that the prices are refreshed every 30 seconds. "))
+        seconds = int(input("Enter the seconds you want to monitor the prices, keep in mind that the prices are refreshed every 10 seconds. "))
         if seconds<30:
             print("\n")
             checkPrices()
             print("\n")
         else:
-            times = seconds / 30
+            times = seconds / 10
             times = int(times)
             for i in range(0,times,1):
                 print("\n")
                 checkPrices()
                 print("\n")
-                time.sleep(30)
+                time.sleep(10)
     elif option == 4:
        global stop
        stop = True
@@ -57,10 +57,13 @@ def checkPrices():
                 inc = ((float(data[i]['price']) / btcprice) - 1 ) * 100
             
             btcprice = float(data[i]['price'])
-            if increment == True:
-                print(str(btcprice) + " ↑ "+colored(str(inc), "green"))
-            elif increment == False:
-                print(str(btcprice) + " ↓ "+colored(str(inc), "red"))
+            if inc != 0:
+                if increment == True:
+                    print("BTC: "+str(btcprice) + " ↑ "+colored(str(inc) + "%", "green"))
+                elif increment == False:
+                    print("BTC: "+str(btcprice) + " ↓ "+colored(str(inc) + "%", "red"))
+            else:
+                print("BTC: "+str(btcprice) + " ~ "+colored(str(inc) + "%", "blue"))
 
         elif i==1:
             if float(data[i]['price']) > ethprice and ethprice != 0: 
@@ -71,17 +74,61 @@ def checkPrices():
                 inc = ((float(data[i]['price']) / ethprice) - 1 ) * 100
 
             ethprice = float(data[i]['price'])
-            if increment == True:
-                print(str(ethprice) + " ↑ "+colored(str(inc), "green"))
-            elif increment == False:
-                print(str(ethprice) + " ↓ "+colored(str(inc), "red"))
+            if inc != 0:
+                if increment == True:
+                    print("ETH: "+str(ethprice) + " ↑ "+colored(str(inc) + "%", "green"))
+                elif increment == False:
+                    print("ETH: "+str(ethprice) + " ↓ "+colored(str(inc) + "%", "red"))
+            else:
+                print("ETH: "+str(ethprice) + " ~ "+colored(str(inc) + "%", "blue"))
         elif i==2:
-            xrpprice = data[i]['price']
+            if float(data[i]['price']) > xrpprice and xrpprice != 0: 
+                increment = True
+                inc = ((float(data[i]['price']) / xrpprice) - 1 ) * 100
+            elif float(data[i]['price']) < xrpprice and xrpprice != 0:
+                increment = False
+                inc = ((float(data[i]['price']) / xrpprice) - 1 ) * 100
+
+            xrpprice = float(data[i]['price'])
+            if inc != 0:
+                if increment == True:
+                    print("XRP: "+str(xrpprice) + " ↑ "+colored(str(inc) + "%", "green"))
+                elif increment == False:
+                    print("XRP: "+str(xrpprice) + " ↓ "+colored(str(inc) + "%", "red"))
+            else:
+                print("XRP: "+str(xrpprice) + " ~ "+colored(str(inc) + "%", "blue"))
         elif i==3:
-            xmrprice = data[i]['price']
+            if float(data[i]['price']) > xmrprice and xmrprice != 0: 
+                increment = True
+                inc = ((float(data[i]['price']) / xmrprice) - 1 ) * 100
+            elif float(data[i]['price']) < xmrprice and xmrprice != 0:
+                increment = False
+                inc = ((float(data[i]['price']) / xmrprice) - 1 ) * 100
+
+            xmrprice = float(data[i]['price'])
+            if inc != 0:
+                if increment == True:
+                    print("XMR: "+str(xmrprice) + " ↑ "+colored(str(inc) + "%", "green"))
+                elif increment == False:
+                    print("XMR: "+str(xmrprice) + " ↓ "+colored(str(inc) + "%", "red"))
+            else:
+                print("XMR: "+str(xmrprice) + " ~ "+colored(str(inc) + "%", "blue"))
         elif i==4:
-            usdtprice = data[i]['price']
-        #print(data[i]['id']+'->'+data[i]['price'])
+            if float(data[i]['price']) > usdtprice and usdtprice != 0: 
+                increment = True
+                inc = ((float(data[i]['price']) / usdtprice) - 1 ) * 100
+            elif float(data[i]['price']) < usdtprice and usdtprice != 0:
+                increment = False
+                inc = ((float(data[i]['price']) / usdtprice) - 1 ) * 100
+
+            usdtprice = float(data[i]['price'])
+            if inc != 0:
+                if increment == True:
+                    print("USTD: "+str(usdtprice) + " ↑ "+colored(str(inc) + "%", "green"))
+                elif increment == False:
+                    print("USTD: "+str(usdtprice) + " ↓ "+colored(str(inc) + "%", "red"))
+            else:
+                print("USTD: "+str(usdtprice) + " ~ "+colored(str(inc) + "%", "blue"))
 
 while stop != True:
     menu()
